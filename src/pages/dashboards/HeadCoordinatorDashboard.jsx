8cc289e594f85e89c335aa742323e6baf41df128
fikre4444 <fikretesfay4444@gmail.com>
2024-09-16 18:02:38 +0300

    finished adding dashboard links to all dashboards except advisor dashboard

--- src/pages/dashboards/HeadCoordinatorDashboard.jsx
+++ src/pages/dashboards/HeadCoordinatorDashboard.jsx
@@ -1,7 +1,33 @@
+import { useState } from "react";
+import SidebarComp from "../../components/Sidebar";
+import { FaRegistered, FaUserCog } from "react-icons/fa";
+import { Outlet } from "react-router-dom";
+
 const HeadCoordinatorDashboard = () => {
+  const [collapsed, setCollapsed] = useState(false);
+  const [toggled, setToggled] = useState(false);
+
+  const sidebarItems = [
+    {itemId: 1, itemTitle: "First head Coodinator page", itemLink: "headCoordinatorPage1", itemIcon: FaUserCog},
+    {itemId: 2, itemTitle: "Second head Coodinator page", itemLink: "headCoordinatorPage2", itemIcon: FaRegistered},
+  ];
+
+
+
   return (
-    <div>
-      HeadCoordinatorDashboard
+    <div className="flex w-full">
+      <SidebarComp 
+        className="bg-blue-500"
+        collapsed={collapsed} setCollapsed={setCollapsed} 
+        toggled={toggled} setToggled={setToggled}
+        sidebarItems={sidebarItems}
+      />
+      <div className="h-[calc(100vh-9vh)] w-full overflow-auto scrollbar-hide">
+        <button className="block md:hidden" onClick={() => setToggled(!toggled)}>
+          toggle
+        </button>
+        <Outlet />
+      </div>
     </div>
   )
 }
--- src/pages/dashboards/StudentDashboard.jsx
+++ src/pages/dashboards/StudentDashboard.jsx
@@ -1,7 +1,33 @@
+import { Outlet } from "react-router-dom";
+import SidebarComp from "../../components/Sidebar";
+import { FaRegistered, FaUserCog } from "react-icons/fa";
+import { useState } from "react";
+
 const StudentDashboard = () => {
+  const [collapsed, setCollapsed] = useState(false);
+  const [toggled, setToggled] = useState(false);
+
+  const sidebarItems = [
+    {itemId: 1, itemTitle: "First Student page", itemLink: "studentPage1", itemIcon: FaUserCog},
+    {itemId: 2, itemTitle: "Second Student page", itemLink: "studentPage2", itemIcon: FaRegistered},
+  ];
+
+
+
   return (
-    <div>
-      StudentDashboard
+    <div className="flex w-full">
+      <SidebarComp 
+        className="bg-blue-500"
+        collapsed={collapsed} setCollapsed={setCollapsed} 
+        toggled={toggled} setToggled={setToggled}
+        sidebarItems={sidebarItems}
+      />
+      <div className="h-[calc(100vh-9vh)] w-full overflow-auto scrollbar-hide">
+        <button className="block md:hidden" onClick={() => setToggled(!toggled)}>
+          toggle
+        </button>
+        <Outlet />
+      </div>
     </div>
   )
 }
