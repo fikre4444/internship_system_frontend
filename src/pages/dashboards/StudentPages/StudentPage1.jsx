637b765c97e1b09da6212773fcb367f55104833f
fikre4444 <fikretesfay4444@gmail.com>
2024-09-26 22:34:08 +0300

    added some experimental code on student dashbaord 1

--- src/pages/dashboards/StudentPages/StudentPage1.jsx
+++ src/pages/dashboards/StudentPages/StudentPage1.jsx
@@ -1,7 +1,23 @@
+import { useState } from 'react';
+
+
 const StudentPage1 = () => {
+  const [ counter, setCounter ] = useState(0);
+
+  const increment1 = () => {
+    setCounter(prevCounter => prevCounter + 1) // or setCounter(prevCounter => prevCounter + 1)
+    setCounter(prevCounter => prevCounter + 1) // or setCounter(prevCounter => prevCounter + 1)
+  }
+  const increment2 = () => {
+    setCounter(counter + 1)
+    setCounter(counter + 1)
+  }
+
   return (
     <div>
-      This is the Student page 1.
+      Value: {counter}
+      <button className="p-2 bg-blue-500 text-xl rounded-lg block m-2" onClick={increment1}>Increment 1</button>
+      <button className="p-2 bg-blue-500 text-xl rounded-lg block m-2" onClick={increment2}>Increment 2</button>
     </div>
   )
 }
