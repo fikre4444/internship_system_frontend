1869a527bed793e4444c22fe676189bfa3ae4748
fikre4444 <fikretesfay4444@gmail.com>
2024-10-26 14:02:16 +0300

    added edit internship for department coordinator

--- src/components/departmentCoordinatorComponents/ViewStudent.jsx
+++ src/components/departmentCoordinatorComponents/ViewStudent.jsx
@@ -19,6 +19,7 @@ const ViewStudent = () => {
   useEffect(() => {
     let account = location?.state;
     setAccount(account);
+    console.log(account.assignedInternship);
   }, [])
 
 
@@ -78,6 +79,8 @@ const StudentPersonalDetails = ({account}) => {
 const InternshipComponent = ({account, setAccount}) => {
   const [internshipInputVisible, setInternshipInputVisible] = useState(false);
 
+  const [editInternshipVisible, setEditInternshipVisible] = useState(false);
+
   const [companyName, setCompanyName] = useState("");
   const [location, setLocation] = useState("");
 
@@ -87,7 +90,7 @@ const InternshipComponent = ({account, setAccount}) => {
   }
 
   return (
-    <div className="flex flex-col gap-3 md:w-min md:items-center m-3 p-2 rounded-md">
+    <div className="flex flex-col gap-3 md:items-center m-3 p-2 rounded-md">
       <h1 className="text-lg font-bold md:text-xl">
         Internship Status
       </h1>
@@ -118,43 +121,52 @@ const InternshipComponent = ({account, setAccount}) => {
               
             </div>
             :
-            <div>
+            !editInternshipVisible ? //if the edit internship is not visible
               <div>
-                <div className="flex gap-0 items-center">
-                  <p className="p-2 w-40 bg-blue-300 bg-opacity-40 text-sm md:text-md m-1 ">
-                    Company Name
-                  </p>
-                  <p className="p-2 w-40 bg-blue-700 bg-opacity-20 text-sm md:text-md m-1">
-                    {account.assignedInternship?.companyName}
-                  </p>
+                <div>
+                  <div className="flex gap-0 items-center">
+                    <p className="p-2 w-40 bg-blue-300 bg-opacity-40 text-sm md:text-md m-1 ">
+                      Company Name
+                    </p>
+                    <p className="p-2 w-40 bg-blue-700 bg-opacity-20 text-sm md:text-md m-1">
+                      {account.assignedInternship?.companyName}
+                    </p>
+                  </div>
+                  <div className="flex gap-0 items-center">
+                    <p className="p-2 w-40 bg-blue-300 bg-opacity-40 text-sm md:text-md m-1 ">
+                      Location
+                    </p>
+                    <p className="p-2 w-40 bg-blue-700 bg-opacity-20 text-sm md:text-md m-1">
+                      {account.assignedInternship?.location}
+                    </p>
+                  </div>
+                  <div className="flex gap-0 items-center">
+                    <p className="p-2 w-40 bg-blue-300 bg-opacity-40 text-sm md:text-md m-1 ">
+                      Type of Internship
+                    </p>
+                    <p className="p-2 w-40 bg-blue-700 bg-opacity-20 text-sm md:text-md m-1">
+                      {account.assignedInternship?.typeOfInternship === "SELF_PROVIDED" ? "Self Provided" : "MU provided"}
+                    </p>
+                  </div>
+                  <div className="flex gap-0 items-center">
+                    <p className="p-2 w-40 bg-blue-300 bg-opacity-40 text-sm md:text-md m-1 ">
+                      Internship Status
+                    </p>
+                    <p className="p-2 w-40 bg-blue-700 bg-opacity-20 text-sm md:text-md m-1 capitalize">
+                      {account.assignedInternshipStatus}
+                    </p>
+                  </div>
                 </div>
-                <div className="flex gap-0 items-center">
-                  <p className="p-2 w-40 bg-blue-300 bg-opacity-40 text-sm md:text-md m-1 ">
-                    Location
-                  </p>
-                  <p className="p-2 w-40 bg-blue-700 bg-opacity-20 text-sm md:text-md m-1">
-                    {account.assignedInternship?.location}
-                  </p>
-                </div>
-                <div className="flex gap-0 items-center">
-                  <p className="p-2 w-40 bg-blue-300 bg-opacity-40 text-sm md:text-md m-1 ">
-                    Type of Internship
-                  </p>
-                  <p className="p-2 w-40 bg-blue-700 bg-opacity-20 text-sm md:text-md m-1">
-                    {account.assignedInternship?.typeOfInternship === "SELF_PROVIDED" ? "Self Provided" : "MU provided"}
-                  </p>
-                </div>
-                <div className="flex gap-0 items-center">
-                  <p className="p-2 w-40 bg-blue-300 bg-opacity-40 text-sm md:text-md m-1 ">
-                    Internship Status
-                  </p>
-                  <p className="p-2 w-40 bg-blue-700 bg-opacity-20 text-sm md:text-md m-1 capitalize">
-                    {account.assignedInternshipStatus}
-                  </p>
-                </div>
+                <Button className="bg-blue-gray-600 rounded-sm my-2"
+                  onClick={() => setEditInternshipVisible(true)}
+                >
+                  Edit Internship
+                </Button>
               </div>
-              <Button className="bg-blue-gray-600 rounded-sm my-2">Edit Internship</Button>
-            </div>
+              :
+              <div>
+                <InternshipInputFormEdit account={account} setAccount={setAccount} setEditInternshipVisible={setEditInternshipVisible} />
+              </div> 
           }
       </div>
     </div>
@@ -205,7 +217,7 @@ const InternshipInputForm = ({account, setAccount}) => {
     setIsSubmitting(true);
     const addingToastId = toast.loading("Adding Internship..");
     toast.update(addingToastId, {closeButton: true});
-    sleep(1000);
+    await sleep(1000);
     try {
       const response = await axios.post("/api/department-coordinator/add-self-internship",
         requestObject, {
@@ -275,6 +287,130 @@ const InternshipInputForm = ({account, setAccount}) => {
         <div className="flex w-max gap-4 m-2 mt-6">
           <Button loading={IsSubmitting} onClick={handleAdd} className="bg-blue-gray-500" >
             {!IsSubmitting ? <>Add Internship</> : <>Adding Internship</>}
+          </Button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+const InternshipInputFormEdit = ({account, setAccount, setEditInternshipVisible}) => {
+  const token = useSelector(state => state.user.token);
+  const companyNameRef = useRef(null);
+  const locationRef = useRef(null);
+
+  const [IsSubmitting, setIsSubmitting] = useState(false);
+
+  const [companyName, setCompanyName] = useState(account.assignedInternship?.companyName);
+  const [location, setLocation] = useState(account.assignedInternship?.location);
+  const [errors, setErrors] = useState({
+    companyName: "", location: ""
+  });
+
+  const validateInputs = () => {
+    if(companyName === null || companyName.trim() === ""){
+      setErrors({...errors, companyName: "Please Input The Company's Name."})
+      return false;
+    }
+    if(location === null || location.trim() === ""){
+      setErrors({...errors, location: "Please Input The Company's Location."})
+      return false;
+    }
+    return true;
+  }
+
+  const handleEdit = async () => {
+    if(!validateInputs()){
+      return;
+    }
+
+    const requestObject = {
+      username: account.username,
+      companyName: companyName,
+      location: location
+    };
+    console.log(requestObject);
+    console.log("Edit internship!");
+
+    setIsSubmitting(true);
+    const editingToastId = toast.loading("Confirming Edit of Internship..");
+    toast.update(editingToastId, {closeButton: true});
+    await sleep(1000);
+    try {
+      const response = await axios.post("/api/department-coordinator/edit-self-internship",
+        requestObject, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        }
+      });
+      if(response.status === 200){
+        console.log(response.data);
+        toast.update(editingToastId, {
+          render: "Successfully Edited The internship!",
+          type: "success",
+          isLoading: false,
+          autoClose: 1000
+        });
+        const addedIntership = response.data.internship;
+        setAccount({...account, assignedInternship: addedIntership, assignedInternshipStatus: "Pending"});
+        setEditInternshipVisible(false);
+      }
+    }catch(error){
+      console.log(error);
+      toast.update(editingToastId, {
+        render: "Some Unknown Error Occured!",
+        type: "error", 
+        isLoading: false,
+        autoClose: 1000
+      });
+    } finally {
+      setIsSubmitting(false);
+    }
+  }
+
+  return (
+    <div>
+      <div className="flex w-72 flex-col gap-2 m-2">
+        <h3 className="text-md text-gray-800 font-extrabold">Company Name</h3>
+        <div className="relative group">
+          <Input
+            color="blue"
+            label="Edit Student's Company Name"
+            value={companyName}
+            onChange={(e) => {
+              setCompanyName(e.target.value);
+              setErrors({...errors, companyName: ""})
+            }}
+            inputRef={companyNameRef}
+          />
+          {errors.companyName && <span className="text-red-500 m-0 p-0 text-sm">{errors.companyName}</span>}
+        </div>
+      </div>
+      <div className="flex w-72 flex-col gap-2 m-2">
+        <h3 className="text-md text-gray-800 font-extrabold">Location</h3>
+        <div className="relative group">
+          <Input
+            color="blue"
+            label="Edit The Company's Location"
+            value={location}
+            onChange={(e) => {
+              setLocation(e.target.value);
+              setErrors({...errors, location: ""})
+            }}
+            inputRef={locationRef}
+          />
+          {errors.location && <span className="text-red-500 m-0 p-0 text-sm">{errors.location}</span>}
+        </div>
+      </div>
+      <div className="">
+        <div className="flex w-max gap-4 m-2 mt-6">
+          <Button loading={IsSubmitting} onClick={handleEdit} className="bg-green-500 rounded-sm my-2" >
+            {!IsSubmitting ? <>Confirm Edit</> : <>Confirming Edit</>}
+          </Button>
+          <Button className="bg-red-500 rounded-sm my-2"
+            onClick={() => setEditInternshipVisible(false)}
+          >
+            Cancel Edit
           </Button>
         </div>
       </div>
